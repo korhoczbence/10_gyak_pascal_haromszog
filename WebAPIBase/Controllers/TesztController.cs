f8249f424230bc0c546cc531f8c3535e025badbd
Körhöcz Bence <korhoczbence@gmail.com>
2023-11-29 16:45:08 +0100

    kerdesek.thml #1. verzió

---
+++ WebAPIBase/Controllers/TesztController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIBase.Controllers
+{
+    [HttpGet]
+    [Route("corvinus/nagybetus/{szoveg}")]
+    public IActionResult M2(string szoveg)
+    {
+        return new ContentResult
+        {
+            ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+            Content = szoveg.ToUpper()
+        };
+    }
+}
